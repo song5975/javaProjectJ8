4976668e48ad3fbac53be903e9d194bbb4616eb2
song5975 <badugi5655@naver.com>
2023-12-01 21:27:19 +0900

    20231201_21:26_JSP

--- src/main/java/user/EmailSendActionCommand.java
+++ src/main/java/user/EmailSendActionCommand.java
@@ -54,7 +54,7 @@ public class EmailSendActionCommand implements UserInterface {
         String to = dao.getUserEmail(userID);
         String subject = "강의평가를 위한 이메일 인증 메일입니다.";
         
-        // 6자리 난수 생성(인증번호)
+        // 6자리 난수 생성(인증번호) / Random 클래스를 사용하여 0 이상 999999 이하의 랜덤한 정수를 생성
         String randomNumber = String.format("%06d", new java.util.Random().nextInt(1000000));
         
         // 세션에 난수 저장
@@ -64,6 +64,7 @@ public class EmailSendActionCommand implements UserInterface {
         
         // 이메일 발송을 위한 JavaMail API 설정 / (SMTP :(Simple Mail Transfer Protocol))
         // Properties 클래스, Java에서 키(key)와 값(value) 쌍으로 이루어진 컬렉션을 관리하기 위한 클래스
+        // Properties 클래스는 컬렉션 프레임워크의 일부로, HashTable을 확장하여 만든 클래스
         Properties p = new Properties();
         // SMTP 서버 인증에 사용될 사용자 이름을 설정, 이메일 발송을 위해 사용되는 계정의 이메일 주소(관리자)
         p.put("mail.smtp.user", from);
@@ -80,6 +81,8 @@ public class EmailSendActionCommand implements UserInterface {
         // SSL/TLS 프로토콜의 버전을 설정( 이름이 "ssl.protocols"이라고 되어 있지만, 실제로는 SSL 및 TLS 프로토콜 버전 모두를 관리하는 속성)
         p.put("mail.smtp.ssl.protocols", "TLSv1.2");
         
+        // Properties 객체는 메일 서버 호스트, 포트, 인증 설정 등 메일 전송에 필요한 환경 설정이 담겨있다.
+        
         /* 	
          	* SMTP 포트
         	- 587 (TLS를 사용하는 경우)
@@ -89,10 +92,6 @@ public class EmailSendActionCommand implements UserInterface {
         	- 현재 TLS를 사용하는 것이 보안 및 호환성 측면에서 권장된다.
         */
 
-        /*
-        	Authenticator 클래스의 getPasswordAuthentication 메서드에서 반환되는 
-        	PasswordAuthentication 객체에는 사용자의 이메일 계정과 해당 이메일 계정에 대한 앱 비밀번호가 포함
-        */
         // Authenticator 는 추상 클래스
         try {
             Authenticator auth = new Authenticator() {
@@ -103,12 +102,34 @@ public class EmailSendActionCommand implements UserInterface {
                     return new PasswordAuthentication(from, "daplpepeofkfamie"); // 생성한 앱 비밀번호
                 }
             };
-            // Properties 객체 p와 Authenticator 객체 auth를 사용하여 세션을 생성
+            
+            /*
+        	Authenticator 클래스의 getPasswordAuthentication 메서드에서 반환되는 
+        	PasswordAuthentication 객체에는 사용자의 이메일 계정과 해당 이메일 계정에 대한 앱 비밀번호가 포함
+        	Authenticator 객체는 사용자의 이메일 주소와 비밀번호 등 메일 서버에 인증 정보를 제공하는 역할
+             */
+            
+            
             // 이 세션은 이메일을 발송하기 위한 전반적인 환경 설정을 가지고 있음
             Session ses = Session.getInstance(p, auth);
             // 세션의 디버그 모드를 활성화
             ses.setDebug(true);
-            // ses 세션을 기반으로 MimeMessage 객체를 생성
+            
+            /*
+            Properties 객체 p와 Authenticator 객체 auth를 사용하여 세션을 생성
+            다른 변수를 사용해도 가능하지만 Session을 생성하는 이유는 Session은 메일 발송을 위한 전반적인 환경 설정을 제공하는데, 
+            이는 메일 서버와의 통신, 디버깅 옵션, 인증 등을 다루고 있어서 이러한 기능을 활용하기 위해 세션으로 생성합니다.
+             */
+            
+            
+            // ses 세션에 담은 정보 기반으로 MimeMessage 객체를 생성
+            
+            /*
+            MimeMessage는 JavaMail API에서 제공하는 클래스로 메일의 주제, 송신자, 수신자 등의 정보를 포함한 헤더를 관리하며,
+            텍스트, HTML, 그림, 첨부 파일 등 다양한 형태의 이메일을 처리할 때 사용한다.
+             
+             */
+            
             // MimeMessage는 MIME(Multipurpose Internet Mail Extensions) 형식의 이메일 메시지를 나타냅니다.
             MimeMessage msg = new MimeMessage(ses);
             // 이메일의 제목을 설정
